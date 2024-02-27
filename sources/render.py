Fix : points projection rotation

sources/render.py
@@ -150,7 +150,7 @@ class Renderer3D:
         assert(self.isRotationMatrix(R))
         sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
         singular = sy < 1e-6
-        if  not singular :
+        if not singular :
             x = math.atan2(R[2,1] , R[2,2])
             y = math.atan2(-R[2,0], sy)
             z = math.atan2(R[1,0], R[0,0])
@@ -166,7 +166,6 @@ class Renderer3D:
         glBegin(GL_POINTS)
         for i, point in enumerate(points):
             color = (0.4, 0.8, 0)
-            #point_wrld = np.dot(rotation, point)
             glColor3f(*color)
             glVertex3f(*point)
         glEnd()

sources/slam.py
@@ -181,7 +181,7 @@ class Slam():
         assert points3D.shape[0] == 3, "Points3D not 3D"
         R_total, t_total = self.vision.get_pose_cumulation()
         pose_corrected = self.vision.camera_pose_to_opengl(t_total, R_total)
-        return np.dot(points3D.T, pose_corrected['R']) + pose_corrected['t'].T
+        return np.dot(points3D.T, -pose_corrected['R']) + pose_corrected['t'].T
     
     # useless
     def project_points(self, points3D):
@@ -209,8 +209,8 @@ class Slam():
         projPoints1 = np.array(projPoints1).T  # (2, N)
         projPoints2 = np.array(projPoints2).T
         K = self.vision.K
-        projMat1 = K @ cv.hconcat([self.vision.current_frame.pose['R'], self.vision.current_frame.pose['t']]) # Cam1 is the origin
-        projMat2 = K @ cv.hconcat([self.vision.last_frame.pose['R'], self.vision.last_frame.pose['t']]) # R, T from stereoCalibrate
+        projMat1 = K @ cv.hconcat([self.vision.current_frame.pose['R'], self.vision.current_frame.pose['t']])
+        projMat2 = K @ cv.hconcat([self.vision.last_frame.pose['R'], self.vision.last_frame.pose['t']])
         points1u = cv.undistortPoints(projPoints1, K, 1, None, K)
         points2u = cv.undistortPoints(projPoints2, K, 1, None, K)
         points4D = cv.triangulatePoints(projMat1, projMat2, points1u, points2u)